140fe2eaa451ff54836e528fa385471b5613d834
AnishaSatapathy01@ <anishasatapathy23@gmail.com>
2025-08-07 22:15:34 +0530

    default constructor

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    int i;
+    String s;
+    public static void main (String args[]){
+        Main obj = new Main();//<--default constructor
+        System.out.println(obj.i+" "+obj.s);
+
+    }
+}
